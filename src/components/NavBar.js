6a29d61d2ac223522a2548d683952023a1900bb5
Rory Blonski <blonski@usc.edu>
2021-04-08 16:16:52 -0400

    added categories styling, fixed map + search bar, added conditional rendering for nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,16 @@ import React from "react"
 import {Link} from "react-router-dom";
 
 class NavBar extends React.Component {
+    
+    constructor(props) {
+        super(props);
+    }
 
     render() {
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand">LAVenture</a>
+                    <a className="navbar-brand" href="#">LAVenture</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                             aria-label="Toggle navigation">
@@ -15,23 +19,30 @@ class NavBar extends React.Component {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto">
-                            <li className="nav-item newActivity">
-                                <Link className="nav-link"
-                                      to={{
-                                          pathname: "/newactivity"
-                                          // state: "" // pass user information to new activity page so DB knows who to
-                                                       // create new activity for
-                                      }}
-                                >
-                                    Create New Activity
-                                </Link>
-                            </li>
-                            <li className="nav-item login-nav">
-                                <Link className="nav-link" to="/login">Login</Link>
-                            </li>
-                            <li className="nav-item logout-nav ms-auto">
-                                <a className="nav-link" >Logout</a>
-                            </li>
+                            { this.props.userLoggedIn &&
+                                <li className="nav-item newActivity">
+                                    <Link className="nav-link"
+                                        to={{
+                                            pathname: "/newactivity"
+                                            // state: "" // pass user information to new activity page so DB knows who to
+                                                        // create new activity for
+                                        }}
+                                    >
+                                        Create New Activity
+                                    </Link>
+                                </li>
+                            }
+                            { !this.props.userLoggedIn &&
+                                <li className="nav-item login-nav">
+                                    <Link className="nav-link" to="/login">Login</Link>
+                                </li>
+                            }
+                            { this.props.userLoggedIn &&
+                                <li className="nav-item logout-nav ms-auto">
+                                    { /* temporary solution for ui purposes*/ }
+                                    <a className="nav-link" onClick={() => this.props.setULI(false)} >Logout</a>
+                                </li>
+                            }
                         </ul>
                     </div>
                 </div>
