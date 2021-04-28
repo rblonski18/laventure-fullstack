d512e095f7979a7d8e167963216c93ab58df8d7a
Rory Blonski <blonski@usc.edu>
2021-04-27 23:32:20 -0400

    RSVP functionality fixed, navbar logout/cookie deletion fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ class NavBar extends React.Component {
     }
 
     deleteCookie = () => {
-        document.cookie = "user=" + this.props.username + ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
+        document.cookie = "user=" + this.props.username + ";expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/";
     }
 
     handleLogout = () => {
